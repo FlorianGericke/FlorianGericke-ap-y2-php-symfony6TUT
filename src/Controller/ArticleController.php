3d0c0a9bb8a1048c815e0fb0abd311e4cf8c3312
fgericke <florian.gericke@endava.com>
2023-07-18 11:54:05 +0200

    using Annotations for Routing and first twig Template

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,18 +2,22 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
-    public function index()
+    #[Route('/', methods:['get'])]
+    final function index() : Response
     {
-        return new Response('
-            <html lang="en">
-                <body>
-                    <div>Hello</div>
-                </body>
-            </html>     
-        ');
+//        return new Response('
+//            <html lang="en">
+//                <body>
+//                    <div>Hello</div>
+//                </body>
+//            </html>
+//        ');
+        return $this->render('articles/index.html.twig');
     }
 }

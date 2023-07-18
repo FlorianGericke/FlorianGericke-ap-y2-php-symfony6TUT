d89aa5ccf50c83d64bfb4fdf7d5ca774f311150d
fgericke <florian.gericke@endava.com>
2023-07-18 13:25:44 +0200

    adding a navbar with Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
         "./templates/**/*.html.twig",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'primary': '#207dea'
+            }
+        },
     },
     plugins: [],
 }

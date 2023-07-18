20a3b1b5983b1adf9593c17da3814a170c462501
fgericke <florian.gericke@endava.com>
2023-07-18 17:17:08 +0200

    adding an article to the database

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,10 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[ORM\Entity]
+#[ORM\Table(name: 'article_controller')]
 class ArticleController extends AbstractController
 {
     #[Route('/', methods: ['get'])]
@@ -21,4 +26,20 @@ class ArticleController extends AbstractController
             'articles' => $articles,
         ));
     }
+
+    // Don't use get to add data to the database. This gets changed to Post later in the tutorial
+    #[Route('/article/save', methods: ['get'])]
+    final function save(EntityManagerInterface $entityManager): Response
+    {
+        $article = new Article();
+        $article->setTitle('Article One');
+        $article->setBody('Body1');
+        $article->setQuality('poor');
+
+        $entityManager->persist($article);
+        $entityManager->flush();
+
+        return new Response('Saved new product with id '.$article->getId());
+    }
+
 }

9fe079fb6ef06365ec6e062907beef39ac193dcb
fgericke <florian.gericke@endava.com>
2023-07-19 11:49:53 +0200

    adding an Input-form

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -7,13 +7,18 @@ use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 
 #[ORM\Entity]
 #[ORM\Table(name: 'article_controller')]
 class ArticleController extends AbstractController
 {
-    #[Route('/', methods: ['get'])]
+    #[Route('/', name: 'article_list', methods: ['get'])]
     final function index(EntityManagerInterface $entityManager): Response
     {
         $articles = $entityManager->getRepository(Article::class)->findAll();
@@ -49,4 +54,37 @@ class ArticleController extends AbstractController
         ));
     }
 
+    #[Route('/article/add', methods: ['GET', 'POST'])]
+    final function addArticle(Request $res, EntityManagerInterface $entityManager): Response
+    {
+        $article = new Article();
+        $form = $this->createFormBuilder($article)
+            ->add('Title', TextType::class, array('attr' =>
+                array('class' => 'form-control')))
+            ->add('Body', TextareaType::class, array('attr' =>
+                array('class' => 'form-control'),
+                'required' => false))
+            ->add('Quality', TextareaType::class, array('attr' =>
+                array('class' => 'form-control'),
+                'required' => false))
+            ->add('Save', SubmitType::class, array('attr' =>
+                array('class' => 'btn-primary'),
+                'label' => 'Create'))
+            ->getForm();
+
+        $form->handleRequest($res);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $article = $form->getData();
+
+            $entityManager->persist($article);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('article_list');
+        }
+
+        return $this->render('articles/new.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
 }
